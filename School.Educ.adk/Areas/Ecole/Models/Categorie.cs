e9009c2a53b76cb583da7c125eb532d36024ea96
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-09 16:34:10 +0200

    Création et implémentation du model de données Categorie

---
+++ School.Educ.adk/Areas/Ecole/Models/Categorie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Areas.Ecole.Models
+{
+    public class Categorie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+        [Required(ErrorMessage = "Le nom est obligatoire")]
+        public string Nom { get; set; }
+    }
+}
