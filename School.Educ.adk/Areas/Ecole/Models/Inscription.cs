c2c5d90da7047b07fa2f42752ac33097f52dd136
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-09 16:27:05 +0200

    Implémentation du model de l'inscription

--- School.Educ.adk/Areas/Ecole/Models/Inscription.cs
+++ School.Educ.adk/Areas/Ecole/Models/Inscription.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,16 @@ namespace School.Educ.adk.Areas.Ecole.Models
 {
     public class Inscription
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+        public string EleveId { get; set; }
+        public string ClasseID { get; set; }
+
+        public DateTime DateInscription { get; set; }
+        public string AnneeScolaire { get; set; }
+
+        public Eleve Eleve { get; set; }
+        public Classe Classe { get; set; }
     }
 }
