5dcd16780389b4fbf2520d3ab47e77fc67ecf2ff
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-09 16:29:03 +0200

    Implémentation du model de cours

--- School.Educ.adk/Areas/Ecole/Models/Cours.cs
+++ School.Educ.adk/Areas/Ecole/Models/Cours.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,14 @@ namespace School.Educ.adk.Areas.Ecole.Models
 {
     public class Cours
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+        public string ClasseID { get; set; }
+
+        public string Intituler { get; set; }
+        public string Categorie { get; set; }
+
+        public Classe Classe { get; set; }
     }
 }
