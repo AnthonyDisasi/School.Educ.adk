66d91c109cc260de41e3dc77384daf7a98b0c746
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-09 15:35:16 +0200

    Création du model classe

---
+++ School.Educ.adk/Areas/Ecole/Models/Classe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Areas.Ecole.Models
+{
+    public class Classe
+    {
+    }
+}
