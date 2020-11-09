f6321cbc4676b5e87bcc686189daadb6a225f7e0
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-09 16:36:29 +0200

    Création du dossier de data pour la dbcontext

---
+++ School.Educ.adk/Areas/Ecole/DataContext/DbEcole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Areas.Ecole.DataContext
+{
+    public class DbEcole : DbContext
+    {
+    }
+}
