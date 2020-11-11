8c46a00915a19444cb18cd6fb63a1e7859d24b91
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-11 12:02:58 +0200

    Création des fichiers pour l'authentification

---
+++ School.Educ.adk/Data/UserAuthent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using School.Educ.adk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Data
+{
+    public class UserAuthent : IdentityDbContext<ApplicationUser>
+    {
+        public UserAuthent(DbContextOptions<UserAuthent> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
