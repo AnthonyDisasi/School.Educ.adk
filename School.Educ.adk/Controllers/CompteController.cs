c35fca32ad3729ba15d5582fe60ea3d17c444d43
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-11 12:43:07 +0200

    Ajout des attribut pour gérer l'authentification, avec injection de dependances

--- School.Educ.adk/Controllers/CompteController.cs
+++ School.Educ.adk/Controllers/CompteController.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using School.Educ.adk.Models;
 
 namespace School.Educ.adk.Controllers
 {
     public class CompteController : Controller
     {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
 
+        public CompteController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
         public IActionResult Index()
         {
             return View();
