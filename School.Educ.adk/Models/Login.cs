bc4b389ce8a3a398bc705f8af0a3ea541a24372f
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-11 12:37:32 +0200

    Création du model de connexion "Login"

---
+++ School.Educ.adk/Models/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "L'identifiant est obligatoire"), Display(Name = "Identifiant")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Le mot de passe est obligatoire"), Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+    }
+}
