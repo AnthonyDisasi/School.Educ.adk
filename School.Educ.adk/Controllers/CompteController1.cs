a6c04ca507954544eb7658fcddee41852e0c779a
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-11 12:39:03 +0200

    Création du controlleur compte pour les connexions , deconnexion et pour l'acces réfusé

---
+++ School.Educ.adk/Controllers/CompteController1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace School.Educ.adk.Controllers
+{
+    public class CompteController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
