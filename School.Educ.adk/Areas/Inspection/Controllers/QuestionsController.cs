ceb5df59750741bd71a878d69c722acec1f063d0
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-12 15:00:31 +0200

    Génération des fichiers pour le model Question du context ExamenDb

---
+++ School.Educ.adk/Areas/Inspection/Controllers/QuestionsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using School.Educ.adk.Areas.Inspection.Data;
+using School.Educ.adk.Areas.Inspection.Models;
+
+namespace School.Educ.adk.Areas.Inspection.Controllers
+{
+    [Area("Inspection")]
+    public class QuestionsController : Controller
+    {
+        private readonly ExamenDb _context;
+
+        public QuestionsController(ExamenDb context)
+        {
+            _context = context;
+        }
+
+        // GET: Inspection/Questions
+        public async Task<IActionResult> Index()
+        {
+            var examenDb = _context.Questions.Include(q => q.Examen);
+            return View(await examenDb.ToListAsync());
+        }
+
+        // GET: Inspection/Questions/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions
+                .Include(q => q.Examen)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
+        // GET: Inspection/Questions/Create
+        public IActionResult Create()
+        {
+            ViewData["ExamenID"] = new SelectList(_context.Examens, "ID", "ID");
+            return View();
+        }
+
+        // POST: Inspection/Questions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,ExamenID,Enoncer,BonneReponse,Cote")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(question);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ExamenID"] = new SelectList(_context.Examens, "ID", "ID", question.ExamenID);
+            return View(question);
+        }
+
+        // GET: Inspection/Questions/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            ViewData["ExamenID"] = new SelectList(_context.Examens, "ID", "ID", question.ExamenID);
+            return View(question);
+        }
+
+        // POST: Inspection/Questions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("ID,ExamenID,Enoncer,BonneReponse,Cote")] Question question)
+        {
+            if (id != question.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(question);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuestionExists(question.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ExamenID"] = new SelectList(_context.Examens, "ID", "ID", question.ExamenID);
+            return View(question);
+        }
+
+        // GET: Inspection/Questions/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions
+                .Include(q => q.Examen)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
+        // POST: Inspection/Questions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+            _context.Questions.Remove(question);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuestionExists(string id)
+        {
+            return _context.Questions.Any(e => e.ID == id);
+        }
+    }
+}
