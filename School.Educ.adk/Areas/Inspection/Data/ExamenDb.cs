9a7f8b75b415d823f4d82282ec4197a227a11cda
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-12 14:23:16 +0200

    Implémentation du context Examen

--- School.Educ.adk/Areas/Inspection/Data/ExamenDb.cs
+++ School.Educ.adk/Areas/Inspection/Data/ExamenDb.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using School.Educ.adk.Areas.Inspection.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace School.Educ.adk.Areas.Inspection.Data
 {
-    public class ExamenDb
+    public class ExamenDb : DbContext
     {
+        public ExamenDb(DbContextOptions<ExamenDb> options) : base(options) { }
+
+        public DbSet<Assertion> Assertions { get; set; }
+        public DbSet<Examen> Examens { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Reponse> Reponses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
