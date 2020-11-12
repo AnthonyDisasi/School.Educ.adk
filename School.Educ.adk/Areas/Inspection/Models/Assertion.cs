b96260081dc2c994ba7bed84acfd5d1c6bb35c60
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-12 14:04:42 +0200

    Implémentation du model Assertion

--- School.Educ.adk/Areas/Inspection/Models/Assertion.cs
+++ School.Educ.adk/Areas/Inspection/Models/Assertion.cs
@@ -7,5 +7,11 @@ namespace School.Educ.adk.Areas.Inspection.Models
 {
     public class Assertion
     {
+        public string ID { get; set; }
+        public string QuestionID { get; set; }
+
+        public string Intituler { get; set; }
+
+        public Question Question { get; set; }
     }
 }
