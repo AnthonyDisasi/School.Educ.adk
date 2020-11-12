0d3e7456451859102d1d9842a5e9809fa05b9db8
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-12 13:53:30 +0200

    Création du model de l'examen

---
+++ School.Educ.adk/Areas/Inspection/Models/Examen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Areas.Inspection.Models
+{
+    public class Examen
+    {
+        public string ID { get; set; }
+
+    }
+}
