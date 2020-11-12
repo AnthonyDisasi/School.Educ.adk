3359bb27434751b06bc3c65b82145e1b0f6d7ea2
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-12 15:33:29 +0200

    Implémentation et création des models dans la zone Professeur

---
+++ School.Educ.adk/Areas/Professeur/Models/Epreuve.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Areas.Professeur.Models
+{
+    public class Epreuve
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+
+        public int EpreuveID { get; set; }
+
+        [Display(Name = "Description")]
+        [StringLength(50, ErrorMessage = "La description doit être plus petit que 50 charactères")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "La période est obligatoire"), Display(Name = "Période")]
+        public string Periode { get; set; }
+
+        public virtual Cotation Cotation { get; set; }
+    }
+}
