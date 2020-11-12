7fb76efa8ffacde6b0f7c4d86550406a6f32fbe2
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-12 14:01:38 +0200

    Implélemetation du model des questions

--- School.Educ.adk/Areas/Inspection/Models/Question.cs
+++ School.Educ.adk/Areas/Inspection/Models/Question.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,15 @@ namespace School.Educ.adk.Areas.Inspection.Models
 {
     public class Question
     {
+        public string ID { get; set; }
+
+        public string ExamenID { get; set; }
+
+        public string Enoncer { get; set; }
+        public string Reponse { get; set; }
+        public double Cote { get; set; }
+
+        public Examen Examen { get; set; }
+        public ICollection<Assertion> Assertion { get; set; }
     }
 }
