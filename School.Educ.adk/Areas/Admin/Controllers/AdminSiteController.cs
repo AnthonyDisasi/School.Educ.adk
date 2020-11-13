203414dab078644ea4a3e8dba7b5b4405992bb7b
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-13 21:40:17 +0200

    Injection des différents attributs

--- School.Educ.adk/Areas/Admin/Controllers/AdminSiteController.cs
+++ School.Educ.adk/Areas/Admin/Controllers/AdminSiteController.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using School.Educ.adk.Models;
 
 namespace School.Educ.adk.Areas.Admin.Controllers
 {
     public class AdminSiteController : Controller
     {
+        private UserManager<ApplicationUser> userManager;
+        private IUserValidator<ApplicationUser> userValidator;
+        private IPasswordValidator<ApplicationUser> passwordValidator;
+        private IPasswordHasher<ApplicationUser> passwordHasher;
+
+        public AdminSiteController(IPasswordHasher<ApplicationUser> _passwordHasher, 
+            IPasswordValidator<ApplicationUser> _passwordValidator,
+            IUserValidator<ApplicationUser> _userValidator,
+            UserManager<ApplicationUser> _userManager)
+        {
+            userManager = _userManager;
+            userValidator = _userValidator;
+            passwordValidator = _passwordValidator;
+            passwordHasher = _passwordHasher;
+        }
+
         public IActionResult Index()
         {
             return View();
