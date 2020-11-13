abf1ad54f73ac24ef5e0dbfa3f542027b040b9d1
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-13 21:27:08 +0200

    création de la classe CustomUserValidator et son implémentation

---
+++ School.Educ.adk/Infrastructure/CustomUserValidator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using School.Educ.adk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Infrastructure
+{
+    public class CustomUserValidator : UserValidator<ApplicationUser>
+    {
+        public async Task<IdentityResult> ValidateAsync(UserManager<ApplicationUser> manager, ApplicationUser user)
+        {
+            IdentityResult result = await base.ValidateAsync(manager, user);
+            List<IdentityError> errors = result.Succeeded ? new List<IdentityError>() : result.Errors.ToList();
+
+            if (!user.Email.ToLower().EndsWith("@example.com"))
+            {
+                errors.Add(new IdentityError
+                {
+                    Code = "EmailDomainError",
+                    Description = "Only example.com email addresses are allowed"
+                });
+            }
+            return errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
+        }
+    }
+}
