7fa17e7732867e6fc202ed61a579b12033c5673a
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-13 21:20:34 +0200

    Implémentation de la classe CustomPasswordValidator

--- School.Educ.adk/Infrastructure/CustomPasswordValidator.cs
+++ School.Educ.adk/Infrastructure/CustomPasswordValidator.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using School.Educ.adk.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace School.Educ.adk.Infrastructure
 {
-    public class CustomPasswordValidator
+    public class CustomPasswordValidator : PasswordValidator<ApplicationUser>
     {
+        public override async Task<IdentityResult> ValidateAsync(UserManager<ApplicationUser> manager, ApplicationUser user, string password)
+        {
+            IdentityResult result = await base.ValidateAsync(manager, user, password);
+            List<IdentityError> errors = result.Succeeded ?
+            new List<IdentityError>() : result.Errors.ToList();
+
+            if (password.ToLower().Contains(user.UserName.ToLower()))
+            {
+                errors.Add(new IdentityError
+                {
+                    Code = "PasswordContainsUserName",
+                    Description = "Le mot de passe  ne peut contenir l'identifiant"
+                });
+            }
+
+            if (password.Contains("12345"))
+            {
+                errors.Add(new IdentityError
+                {
+                    Code = "PasswordContainsSequence",
+                    Description = "Le mot de passe ne peut contenir des chiffres numeriques"
+                });
+            }
+
+            return errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
+        }
     }
 }
