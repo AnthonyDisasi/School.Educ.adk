f73b69d12370a7a8a859eec4be2d1cb8271d9176
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-13 12:21:06 +0200

    Implémentation des models echange et lecon

---
+++ School.Educ.adk/Areas/Professeur/Models/Echange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Areas.Professeur.Models
+{
+    public class Echange
+    {
+        public string ID { get; set; }
+
+        public string LeconID { get; set; }
+        public string Inspecteur { get; set; }
+
+        public double Cotation { get; set; }
+        public string Remarque { get; set; }
+
+        public Lecon Lecon { get; set; }
+    }
+}
