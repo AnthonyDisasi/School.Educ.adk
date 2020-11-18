19731321404d48b95f92c4b0ce7ee4ebe94abced
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-18 15:49:10 +0200

    Ajout de l'injection de dependance dans le controlleur des roles pour admin

--- School.Educ.adk/Areas/Admin/Controllers/RoleAdminSiteController.cs
+++ School.Educ.adk/Areas/Admin/Controllers/RoleAdminSiteController.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using School.Educ.adk.Models;
 
 namespace School.Educ.adk.Areas.Admin.Controllers
 {
     public class RoleAdminSiteController : Controller
     {
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUser> userManager;
+
+        public RoleAdminSiteController(RoleManager<IdentityRole> _roleManager, UserManager<ApplicationUser> _userManager)
+        {
+            roleManager = _roleManager;
+            userManager = _userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
