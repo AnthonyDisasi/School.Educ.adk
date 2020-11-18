af46ad2d49348f129449a048afe6345a9ebebcf4
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-18 17:33:03 +0200

    Création et implementation du model Role

---
+++ School.Educ.adk/Models/RoleEditModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Educ.adk.Models
+{
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+    }
+}
