be68e5972746b0efdd7a2171611bbf00cafdd180
AnthonyDisasi <anthonydisasi@gmail.com>
2021-02-16 09:23:01 +0200

    Ajout des dataAnotations du modele Lecon

--- School.Educ.adk/Areas/Ecole/Models/Lecon.cs
+++ School.Educ.adk/Areas/Ecole/Models/Lecon.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,17 @@ namespace School.Educ.adk.Areas.Ecole.Models
 {
     public class Lecon
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string ID { get; set; }
 
         public string ProfesseurID { get; set; }
+        [Required(ErrorMessage = "Le cours est obligatoire"), Display(Name = "Intituler du cours")]
         public string CoursID { get; set; }
 
+        [Required(ErrorMessage = "La leçon est obligatoire"), Display(Name = "Leçon")]
         public string LeconDonnee { get; set; }
+        [Required(ErrorMessage = "La date est obligatoire"), Display(Name = "Date de leçon")]
         public DateTime DateLecon { get; set; }
 
         public virtual Professeur Professeur { get; set; }
